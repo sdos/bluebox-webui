22e533a4c913b3823d82e4139cb42be1ab43505b
Arthur Kaul <arthur,kaul@gmx.de>
2015-11-24 14:41:41 +0100

    Content Type Converter: transform the Data Type names

---
+++ angular/modules/container/dataTypeConverter.js
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+
+"use strict";
+
+containerModule.filter('contentType', function() {
+	return function(input) {
+		input = input + "";
+		var splitArray = input.split("/");
+		var result;
+		if (splitArray[0] == "text"){
+			result = "Text";
+		}else if(splitArray[0] == "image"){
+			result = "Image";
+		}else if(splitArray[0] == "application"){
+			result = "Application";
+		}else if(splitArray[0] == "audio"){
+			result = "Audio";
+		}else if(splitArray[0] == "video"){
+			result = "Video";
+		}else if(splitArray[0] == "multipart"){
+			result = "Multipart";
+		}else if(splitArray[0] == "message"){
+			result = "Message";
+		}else if(splitArray[0] == "model"){
+			result = "Model";
+		}else{
+			result = "Not Defined";
+		}			
+		
+		return result;
+	}
+});
