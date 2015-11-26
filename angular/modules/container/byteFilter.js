cdb07c451561b09615a9a2bba964c0d32a65e865
Arthur Kaul <arthur,kaul@gmx.de>
2015-11-26 20:12:10 +0100

    Refactor for file names of filters + consistent names for type size

--- angular/modules/container/byteFilter.js
+++ angular/modules/container/byteFilter.js
@@ -9,7 +9,7 @@ containerModule.filter('bytes', function() {
 	return function(input, precision) {
 		if (isNaN(parseFloat(input)) || !isFinite(input)) return '-';
 		if (typeof precision === 'undefined') precision = 2;
-		var units = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB'],
+		var units = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB'],
 			number = Math.floor(Math.log(input) / Math.log(1024));
 		var result = (input / Math.pow(1024, Math.floor(number))).toFixed(precision);
 		if (isNaN(result)) return "0.00" + " " + units[0];
