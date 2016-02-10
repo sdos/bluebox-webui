6e54f3d43834112685877d347f8c04dac2b422b3
Tim Waizenegger <tim.waizenegger@ipvs.uni-stuttgart.de>
2016-02-10 16:55:54 +0100

    add new example config, remove old internal config

---
+++ appConfig.example.py
@@ -0,0 +1,26 @@
+"""
+	Project Bluebox
+	2015, University of Stuttgart, IPVS/AS
+"""
+""" 
+	Project Bluebox 
+	
+	Copyright (C) <2015> <University of Stuttgart>
+	
+	This software may be modified and distributed under the terms
+	of the MIT license.  See the LICENSE file for details.
+"""
+#swift_type = "BluemixV1Auth"
+#swift_url = "https://swift.ng.bluemix.net/global_auth/631afd9d-89a3-4bc7-b1e4-7e1723c6195a/a898adc0-770a-4c6a-874b-3f9dc353dabb"
+#swift_user = "1624de7f5cbaa7b897ec6abd03a80ac59c3d2547"
+#swift_pw = "ef6fd13111aaab99d94a0c9cf8daa05d4ebd2b0fa007ff819637f96dc8c9"
+"""
+swift_type = "BluemixV1Auth"
+swift_url ="https://swift.ng.bluemix.net/global_auth/f510262a-38f3-4489-b250-15fe8927066f/e9a28ae3-0c75-4b96-8f3d-238f63efe93c"
+swift_user = "5eedc87ad5782a811406fc45a5cd26b01d07a492"
+swift_pw = "dbc1a784e0269c38b7908c3d2a9d921a19fa85be80cd185d088b200b7812"
+"""
+swift_type = "regular"
+swift_url = "http://192.168.209.204:8080/auth/v1.0"
+swift_user = "test:tester"
+swift_pw = "testing"
