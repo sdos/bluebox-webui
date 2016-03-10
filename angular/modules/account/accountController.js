61b15ba0f0134db51d24422fe92cbd693e37a024
Tim Waizenegger <tim.waizenegger@ipvs.uni-stuttgart.de>
2016-03-10 09:21:15 +0100

    code format

--- angular/modules/account/accountController.js
+++ angular/modules/account/accountController.js
@@ -4,17 +4,23 @@
  * AccountController
  * controller for "account"
  */
-accountModule.controller('AccountController',
-    ['$scope', '$rootScope', '$state', '$stateParams', '$timeout', '$filter', '$cookies',
-        function($scope, $rootScope, $state, $stateParams, $timeout, $filter, $cookies) {
+accountModule.controller('AccountController', [
+		'$scope',
+		'$rootScope',
+		'$state',
+		'$stateParams',
+		'$timeout',
+		'$filter',
+		'$cookies',
+		function($scope, $rootScope, $state, $stateParams, $timeout, $filter,
+				$cookies) {
 
-    	console.log("nothing to do here...");
-    	
-    	$scope.forgetToken = function() {
-    		console.log("bye bye token...");
-    		$cookies.remove('XSRF-TOKEN');
-    		
-    		
-    	};
-            
-        }]);
+			console.log("nothing to do here...");
+
+			$scope.forgetToken = function() {
+				console.log("bye bye token...");
+				$cookies.remove('XSRF-TOKEN');
+
+			};
+
+		} ]);
