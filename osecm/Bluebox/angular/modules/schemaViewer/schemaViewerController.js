596654f9ae6f98875a11d55b3b8f70c5aaff0aa8
christophtrybek <christoph.trybek@gmail.com>
2016-04-21 14:54:58 +0200

    Added schemaViewer Module

---
+++ osecm/Bluebox/angular/modules/schemaViewer/schemaViewerController.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * AboutController
+ * controller for "about"
+ */
+schemaViewerModule.controller('schemaViewerController',
+    ['$scope', '$rootScope', '$state', '$stateParams', '$timeout', '$filter',
+        function($scope, $rootScope, $state, $stateParams, $timeout, $filter) {
+
+    	console.log("load data Table...")
+            
+        }]);
