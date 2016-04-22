0fb08a81a11ab0df9ccd199df33b130578c14ca1
Tim Waizenegger <tim.waizenegger@ipvs.uni-stuttgart.de>
2016-04-22 16:33:37 +0200

    content type icons

--- osecm/Bluebox/angular/modules/filter/filter.js
+++ osecm/Bluebox/angular/modules/filter/filter.js
@@ -9,4 +9,13 @@ var filterModule = angular.module('bluebox.filter', [
 	return function(input) {
 		return input.replace("x-object-meta-filter-", "");
 	}
+}).filter('fileTypeIcon', function() {
+	return function(input) {
+		if(input.startsWith("image/")) 						return "image";
+		if(input.startsWith("text/plain"))					return "description";
+		if(input.startsWith("application/pdf")) 			return "picture_as_pdf";
+		if(input.startsWith("message/rfc822")) 				return "email";
+		if(input.startsWith("application/octet-stream")) 	return "help";
+		return "insert_drive_file";
+	}
 });
