16969c8aa0f59ad09e4058b06066f152ae3de3bf
Tim Waizenegger <tim.waizenegger@ipvs.uni-stuttgart.de>
2016-10-06 18:11:52 +0200

    task feature UI and service

--- mcm/Bluebox/TasksServer.py
+++ mcm/Bluebox/TasksServer.py
@@ -3,24 +3,44 @@
 """
 	Project Bluebox
 
-	Copyright (C) <2015> <University of Stuttgart>
+	Copyright (C) <2016> <University of Stuttgart>
 
 	This software may be modified and distributed under the terms
 	of the MIT license.  See the LICENSE file for details.
 """
-from mcm.Bluebox import app#, socketio
-from flask import Response
-#import eventlet
-#from flask_socketio import SocketIO, emit, join_room, leave_room, close_room, rooms, disconnect
+import collections
+from io import StringIO
+from datetime import datetime
+from functools import wraps
+import json, logging, time, re
+from urllib import parse as urlParse
+
+from bokeh.charts import Area, show, vplot, output_file, Bar, Line, BoxPlot, defaults
+from bokeh.io import vform
+from bokeh.embed import components
+from bokeh.charts.operations import blend
+from flask import request, Response, send_file, render_template
+import requests
+
+from mcm.Bluebox import app
+from mcm.Bluebox import appConfig
+from mcm.Bluebox.exceptions import HttpError
+import pandas
+from bokeh.models.tickers import SingleIntervalTicker
+from bokeh.models.axes import LinearAxis
+
+import sqlite3
 
 
-# @socketio.on('message')
-# def handle_message(message):
-# 	print('received message: ' + message)
-# 	
-# 	
-# 	
-# @app.route("/messaging/test", methods=["GET"])	
-# def publish():
-# 	socketio.emit("e1", "hallo")
-# 	return "OK"
+logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(module)s - %(levelname)s ##\t  %(message)s")
+log = logging.getLogger()
+
+valid_task_types = {"identify_content": "Identify content types",
+						"extract_metadata": "Extract metadata",
+						"replicate_metadata": "Replicate metadata",
+						"disposal": "Dispose old objects"}
+
+
+@app.route("/api_tasks/types", methods=["GET"])
+def get_valid_tasks():
+	return Response(json.dumps(valid_task_types), mimetype="application/json")
--- mcm/Bluebox/angular/modules/tasks/tasksController.js
+++ mcm/Bluebox/angular/modules/tasks/tasksController.js
@@ -5,9 +5,83 @@
  * controller for the view of tasks
  */
 tasksModule.controller('TasksController',
-		  ['$scope', '$rootScope', '$state', '$stateParams', '$timeout', '$filter',
-	        function($scope, $rootScope, $state, $stateParams, $timeout, $filter) {
+    ['$scope', '$rootScope', '$state', '$stateParams', '$timeout', '$filter', '$http', 'fileSystemService',
+        function ($scope, $rootScope, $state, $stateParams, $timeout, $filter, $http, fileSystemService) {
 
-	    	console.log("nothing to do here...")
-	            
-	        }]);
+            console.log("tasks!")
+
+            updateValidTasks();
+
+            $scope.validTasks = {"no": "...not loaded..."};
+            $scope.availableContainers = undefined;
+
+            $scope.newTaskDefinition = {
+                "type": "",
+                "container": "",
+                "user": "",
+                "token": ""
+            };
+
+            /**
+             *
+             * Get the list of valid tasks
+             *
+             * */
+            $scope.updateValidTasks = updateValidTasks;
+            function updateValidTasks() {
+                $http
+                    .get('api_tasks/types')
+                    .then(
+                        function successCallback(response) {
+                            //console.log(response.data);
+                            $scope.validTasks = response.data;
+
+                            if (!response.data) {
+                                $rootScope
+                                    .$broadcast(
+                                        'FlashMessage',
+                                        {
+                                            "type": "danger",
+                                            "text": "unable to retrieve task list..."
+                                        });
+                            }
+                        },
+                        function errorCallback(response) {
+                            console
+                                .log(JSON
+                                    .stringify(response));
+                            $rootScope
+                                .$broadcast(
+                                    'FlashMessage',
+                                    {
+                                        "type": "danger",
+                                        "text": "Error: "
+                                        + response.data
+                                    });
+                        });
+            };
+
+            /**
+             *
+             * Get the list of container from swift
+             *
+             * */
+
+            fileSystemService.getContainers("", "", 10000)
+                .then(function (response) {
+                    $scope.availableContainers = response.containers;
+                    console.log($scope.availableContainers);
+                })
+                .catch(function (response) {
+                    if (401 == response.status) {
+                        $state.go('loginState', {noAuth: true});
+                        return;
+                    }
+                    $rootScope.$broadcast('FlashMessage', {
+                        "type": "warning",
+                        "text": response.data
+                    });
+                });
+
+
+        }]);
