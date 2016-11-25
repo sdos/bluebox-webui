c0087e0a955d5dba07072efa9a518c4ab256019b
Tim Waizenegger <tim.waizenegger@ipvs.uni-stuttgart.de>
2016-11-25 22:17:18 +0100

    make audio great again!

--- mcm/Bluebox/angular/modules/filter/filter.js
+++ mcm/Bluebox/angular/modules/filter/filter.js
@@ -11,6 +11,7 @@ var filterModule = angular.module('bluebox.filter', []).filter('metaPrefix', fun
 }).filter('fileTypeIcon', function () {
     return function (input) {
         if (input.startsWith("image/"))                        return "image";
+        if (input.startsWith("audio/"))                        return "audiotrack";
         if (input.startsWith("text/plain"))                    return "description";
         if (input.startsWith("application/pdf"))            return "picture_as_pdf";
         if (input.startsWith("message/rfc822"))                return "email";
