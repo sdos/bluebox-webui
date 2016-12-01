05f04102e38ec1de01306938db113c2a83e49e8e
Tim Waizenegger <tim.waizenegger@ipvs.uni-stuttgart.de>
2016-12-01 18:34:13 +0100

    use colored logs

--- mcm/Bluebox/appConfig.example.py
+++ mcm/Bluebox/appConfig.example.py
@@ -23,7 +23,6 @@ this is the current config file for bluebox.
 # log_level = logging.WARNING
 # log_level = logging.INFO
 log_level = logging.DEBUG
-log_format = '%(asctime)s - %(module)s - %(levelname)s ##\t  %(message)s'
 
 """
 ################################################################################
--- mcm/__init__.py
+++ mcm/__init__.py
@@ -8,6 +8,16 @@
 	This software may be modified and distributed under the terms
 	of the MIT license.  See the LICENSE file for details.
 """
-import logging
+import coloredlogs
 from mcm.Bluebox import appConfig
-logging.basicConfig(level=appConfig.log_level, format=appConfig.log_format)
+
+log_format = '%(asctime)s %(module)s %(name)s[%(process)d] %(levelname)s %(message)s'
+field_styles = {'module': {'color': 'magenta'}, 'hostname': {'color': 'magenta'}, 'programname': {'color': 'cyan'},
+                'name': {'color': 'blue'}, 'levelname': {'color': 'black', 'bold': True}, 'asctime': {'color': 'green'}}
+
+coloredlogs.install(level=appConfig.log_level, fmt=log_format, field_styles=field_styles)
+
+"""
+import logging
+logging.basicConfig(level=appConfig.log_level, format=log_format)
+"""
