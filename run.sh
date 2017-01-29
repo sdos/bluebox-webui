c4654830d770c64167137f035605708d35f159eb
Tim Waizenegger <tim.waizenegger@ipvs.uni-stuttgart.de>
2017-01-29 16:10:51 +0100

    cascade locking

--- run.sh
+++ run.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+#	Project MCM
+#
+#	Copyright (C) <2015-2017> Tim Waizenegger, <University of Stuttgart>
+#
+#	This software may be modified and distributed under the terms
+#	of the MIT license.  See the LICENSE file for details.
+
 
 source setenv.sh
 
--- setenv.sh
+++ setenv.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-
+#	Project MCM
+#
+#	Copyright (C) <2015-2017> Tim Waizenegger, <University of Stuttgart>
+#
+#	This software may be modified and distributed under the terms
+#	of the MIT license.  See the LICENSE file for details.
 
 p=$(pwd)
 export PYTHONPATH=$PYTHONPATH:$p/mcm
